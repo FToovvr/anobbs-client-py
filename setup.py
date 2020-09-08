4de8a2706952edbe3a841a4cafe0ebc0b7ca699d
FToovvr <FToovvr@protonmail.com>
2020-09-08 21:22:18 +0800

    build: 配置 pypi

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="anobbs-api-wrapper-py",
+    version="0.0.0-dev1",
+    author="FToovvr",
+    author_email="FToovvr@protonmail.com",
+    description="Simple AnoBBS API wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FToovvr/anobbs-api-wrapper-py",
+    packages=setuptools.find_packages(exclude=("test",)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Natural Language :: Chinese (Simplified)",
+    ],
+    python_requires='>=3.8',
+)
