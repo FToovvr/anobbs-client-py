0743fb11015602b75d4c2ff838812dff7fe9cb2b
FToovvr <FToovvr@protonmail.com>
2021-03-13 12:42:29 +0800

    build: Bump pypi 包的版本至 `0.0.3`

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="anobbs-client",
-    version="0.0.2",
+    version="0.0.3",
     author="FToovvr",
     author_email="FToovvr@protonmail.com",
     description="Simple AnoBBS API wrapper",
